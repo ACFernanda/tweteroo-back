caf1dd42ad76eb1fced369871078d7bc27c16a3b
acfernanda <acfernanda98@gmail.com>
2022-04-21 12:03:35 -0300

    sign up working

--- app.js
+++ app.js
@@ -5,8 +5,21 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 
-const users = [];
-const tweets = [];
+const users = [
+  {
+    username: "bobesponja",
+    avatar:
+      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+  },
+];
+const tweets = [
+  {
+    username: "bobesponja",
+    avatar:
+      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+    tweet: "eu amo o hub",
+  },
+];
 
 app.post("/sign-up", (req, res) => {
   const user = req.body;
@@ -21,7 +34,11 @@ app.post("/tweets", (req, res) => {
 });
 
 app.get("/tweets", (req, res) => {
-  res.send(tweets.slice((tweets[tweets.length - 1], -10)));
+  if (tweets.length > 10) {
+    res.send(tweets.slice((tweets[tweets.length - 1], -10)));
+  } else {
+    res.send(tweets);
+  }
 });
 
 app.listen(5000, () => console.log(chalk.green("Rodando na porta 5000.")));
